4af2921058534daf8f260b946d4eaf80443733a9
just-me- <hess-marcel@bluewin.ch>
2018-03-11 15:56:28 +0100

    2 neue Rezepte (Bolognese + Salatsauce)

---
+++ templates/recipe_bolognese.php
@@ -0,0 +1,50 @@
+<div id="main_recipe_bolognese" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Spaghetti Bolognese</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>1kg Rindhackfleisch</li>
+                <li>2 grosse Zwiebel</li>
+                <li>5 Knoblauchzehen</li>
+                <li>5dl Tomatensauce</li>
+                <li>1 kleine Dose Tomatenmark</li>
+                <li>Öl zum anbraten</li>
+                <li>Etwas Salz und Zucker</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/bolognese_main.jpg" alt="Spaghetti Bolognese" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Die <span class="highlight">Zwiebeln</span> auf <span class="highlight">hoher Stufe in Öl</span> anbraten.</li>
+        <li>Das <span class="highlight">Fleisch</span> dazugeben und gut anbraten.</li>
+        <li>
+            In der Pfanne ein <span class="highlight">kleines Loch</span> freimachen und das <span class="highlight">Tomatenmark</span> hineingeben.
+            Dazu <span class="highlight">2 EL Zucker</span> und <span class="highlight">etwas Salz</span>.
+        </li>
+        <li><span class="highlight">Knoblauch</span> dazugeben, alles vermischen und die <span class="highlight">Hitze etwas zurückstellen</span>.</li>
+        <li>Die <span class="highlight">Tomatensauce</span> in die Pfanne geben, die Flasche mit ca. <span class="highlight">1dl Wasser</span> füllen, schütteln und ebenfalls hinzuleeren.</li>
+        <li>Mit <span class="highlight">Salz und Zucker</span> abschmecken.</li>
+        <li>Bei <span class="highlight">kleinster Stufe für zwei Stunden</span> ziehen lassen.</li>
+    </ol>
+    
+</div>
---
+++ templates/recipe_salatsauce.php
@@ -0,0 +1,45 @@
+<div id="main_recipe_salatsauce" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Französische Salatsauce</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>2 EL Essig</li>
+                <li>1 EL Mayonnaise</li>
+                <li>1 TL Senf</li>
+                <li>4 EL Olivenöl</li>
+                <li>1 Zwiebel</li>
+                <li>2 Knoblauchzehen</li>
+                <li>Schnittlauch</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/salatsauce_main.jpg" alt="Salatsauce" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>
+            Die <span class="highlight">Zwiebel</span> und den <span class="highlight">Schnittlauch</span> hacken,
+            den <span class="highlight">Knoblauch</span> pressen oder fein schneiden und mit den restlichen Zutaten vermengen.
+        </li>
+        <li>Mit <span class="highlight">Salz und Pfeffer</span> abschmecken.</li>
+    </ol>
+    
+</div>
