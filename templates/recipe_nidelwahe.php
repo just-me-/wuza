7e2732b6395751647727d259b01135d608bc171e
just-me- <hess-marcel@bluewin.ch>
2018-04-15 14:01:34 +0200

    Weitere Rezepte (Nidelwähe + Süsssauer)

---
+++ templates/recipe_nidelwahe.php
@@ -0,0 +1,64 @@
+<div id="main_recipe_nidelwahe" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Nidelwähe</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten für den Teig</h2>
+            <ul class="ingredients checkable noselect">
+                <li>1.5dl Milch</li>
+                <li>1 Ei</li>
+                <li>1 Prise Salz</li>
+                <li>4 EL Öl</li>
+                <li>60g Zucker</li>
+                <li>350g Mehl</li>
+                <li>2 TL Backpluver</li>
+                <li>(Alternative: fertiger Butterteig)</li>
+            </ul>
+            
+            <h2>Zutaten für die Füllung</h2>
+            <ul class="ingredients checkable noselect">
+                <li>3.5dl Vollrahm</li>
+                <li>2 Eigelb</li>
+                <li>60g Zucker + etwas mehr zum Bestreuen</li>
+                <li>Etwas Butter</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/nidelwahe_main.jpg" alt="Nidelwähe" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/nidelwahe_1.jpg" alt="Geschnitten" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselectlal">
+        <li>Für den <span class="highlight">Teig</span> alle Zutaten unterkneten (das Backpulver erst nach dem Mehl).</li>
+        <li>Dann muss der Teig für <span class="highlight">15 Minuten</span> ruhen. Im Anschluss auf ein paar wenige Millimeter
+        <span class="highlight">ausrollen</span>. (Lieber etwas zu dünn anstelle zu dick.)</li>
+        <li>In eine <span class="highlight">Kuchenform</span> ein Backpapier auslegen und dann den Teig darüber ausbreiten.
+            Mit einer <span class="highlight">Gabel</span> den Boden einstechen. 
+            (Achtung: die Füllung soll nicht beim Backen auslaufen können).
+        </li>
+        <li>Den Backofen auf <span class="highlight">180°C</span> aufheizen und den Teig schon mal etwas <span class="highlight">vorbacken</span>. (max. 10 Minuten)</li>
+        <li>Für die Füllung alle Zutaten <span class="highlight">cremig schlagen</span>. Die Kuchenform wieder aus dem Ofen nehmen und die Füllung hineinleeren.</li>
+        <li>Nun ein paar <span class="highlight">Butterflocken</span> darin verteilen und zum Abschluss mit noch etwas <span class="highlight">Zucker</span> bestreuen.</li>
+        <li>Im Ofen nun <span class="highlight">40 Minuten</span> (oder etwas weniger - bis der Teig schön golden ist) backen.</li>
+        <li>Vor dem Servieren <span class="highlight">auskühlen</span> lassen; erst dann wird die Füllung fest.</li>
+    </ol>
+    
+</div>
---
+++ templates/recipe_susssauer.php
@@ -0,0 +1,68 @@
+<div id="main_recipe_susssauer" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Chinesisch Süss-Sauer</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Poulet</li>
+                <li>2 Zwiebeln</li>
+                <li>3 Peperoni</li>
+                <li>(Karotten, Bambussprossen)</li>
+                <li>6EL Apfelessig</li>
+                <li>4El Ketchup</li>
+                <li>1El Sojasauce</li>
+                <li>2El Stärkemehl (Maisstärke)</li>
+                <li>Eingelegte Ananas, geschnitten (ca. 270g)</li>
+                <li>150g Zucker</li>
+                <li>3dl Wasser</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/susssauer_main.jpg" alt="Süss-Sauer" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/susssauer_step1.jpg" alt="Gemüse anbraten" />
+                    <span class="title">Das Gemüse anbraten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/susssauer_step2.jpg" alt="Sauce anrühren" />
+                    <span class="title">Die Sauce separat anrühren</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/susssauer_step3.jpg" alt="Köcheln lassen" />
+                    <span class="title">Alles gemeinsam köcheln lassen</span>
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Das <span class="highlight">Poulet</span> mit etwas Öl anbraten.</li>
+        <li>Das Fleisch zur Seite stellen und dann das <span class="highlight">Gemüse</span> anbraten.</li>
+        <li>In einen <span class="highlight">separaten Topf</span> den <span class="highlight">Zucker</span>,
+            eine <span class="highlight">Prise Salz</span> und ca. <span class="highlight">2/3 des Wassers</span> geben.
+            Auf der Herdplatte erwärmen und den <span class="highlight">Apfelessig, Ketchup und die Sojasauce</span> hinzufügen.
+            Umrühren, bis sich alles aufgelöst hat.
+            Dann die <span class="highlight">Ananasstücke inklusive Saft</span> hinzugeben. 
+        </li>
+        <li>Zu dem <span class="highlight">restlichen Wasser</span> die <span class="highlight">Maisstärke</span> unterrühren.</li>
+        <li>Das Gemisch der Sauce hinzuschütten und nochmals <span class="highlight">aufkochen</span> lassen.</li>
+        <li>Nun die <span class="highlight">Sauce mit dem Gemüse und dem Fleisch</span> verrühren und noch etwas weiter köcheln lassen.</li>
+        <li>Am besten mit <span class="highlight">Jasminreis</span> servieren.</li>
+    </ol>
+    
+</div>
