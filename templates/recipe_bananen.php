8b89c4d6b5d5d189833a11b99384d4f0fb189c81
just-me- <hess-marcel@bluewin.ch>
2018-05-07 23:25:41 +0200

    Weitere Rezepte (Gekochte Bananen und Creme)

---
+++ templates/recipe_bananen.php
@@ -0,0 +1,55 @@
+<div id="main_recipe_bananen" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Gekochte Bananen</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>50g Butter</li>
+                <li>160g Rohrzucker</li>
+                <li>½ TL Zimt</li>
+                <li>1dl Rum</li>
+                <li>4 Bananen</li>
+                <li>4 Kugeln Vanille Eis</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/bananen_main.jpg" alt="Gekochte Bananen" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/bananen_step1.jpg" alt="Zucker karamellisieren mit Butter" />
+                    <span class="title">Zucker karamellisieren</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/bananen_step2.jpg" alt="Bananen kochen" />
+                    <span class="title">Bananen kurz aufkochen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/bananen_step3.jpg" alt="Warm servieren" />
+                    <span class="title">Noch warm mit Eis servieren</span>
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Den <span class="highlight">Zucker</span>, <span class="highlight">Zimt</span> und die <span class="highlight">Butter</span> erhitzen bis der Zucker karamellisiert.</li>
+        <li>Die Masse kurz etwas abkühlen lassen und dann den kalten (damit der Alkohol nicht komplett verdampft) <span class="highlight">Rum</span> dazugeben.</li>
+        <li>Die <span class="highlight">geviertelten Bananen</span> in die heisse - aber nicht kochende - Masse geben und leicht weichkochen.</li>
+        <li>Auf die <span class="highlight">Eiskugeln</span> die warme Bananenmasse geben und sofort servieren.</li>
+    </ol>
+    
+</div>
---
+++ templates/recipe_creme.php
@@ -0,0 +1,51 @@
+<div id="main_recipe_creme" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Crème</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>6dl Milch</li>
+                <li>2 Eier</li>
+                <li>3EL Zucker</li>
+                <li>1EL Stärkemehl</li>
+                <li>1 Vanilleschote oder 90g dunkle Schokolade</li>
+                <li>(Himbeeren)</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/creme_main.jpg" alt="Crème" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/creme_step1.jpg" alt="Gekochte Beeren" />
+                    <span class="title">Hervorragend mit Früchten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/creme_step2.jpg" alt="Serviert" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Falls Schokoladencrème: Die Schokolade vorher im Wasserbad <span class="highlight">schmelzen</span>.</li>
+        <li><span class="highlight">Alle Zutaten</span> in eine Pfanne geben und unter <span class="highlight">ständigem Rühren</span> zum Kochen bringen.</li>
+        <li>Einige Minuten weiterkochen und dann in ein <span class="highlight">kühles Gefäss</span> leeren.</li>
+        <li>Die Crème <span class="highlight">abkühlen lassen</span> und immer wieder etwas <span class="highlight">umrühren</span>, dass sich keine Haut bildet.</li>
+        <li>Ist die Crème nicht mehr warm; zum Auskühlen in den <span class="highlight">Kühlschrank</span> geben.</li>
+    </ol>
+    
+</div>
