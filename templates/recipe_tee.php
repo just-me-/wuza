77731418d38529bcf0bd5207b3e54eb7cfbdf0cb
just-me- <hess-marcel@bluewin.ch>
2018-11-03 12:39:53 +0100

    Rezept: Persischer Tee (Mischung)

---
+++ templates/recipe_tee.php
@@ -0,0 +1,42 @@
+<div id="main_recipe_tee" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Persischer Schwarztee</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>100g Earl Grey</li>
+                <li>100g Darjeeling</li>
+                <li>50g Kardamom</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/tee_main.jpg" alt="Tasse Tee" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/tee_end.jpg" alt="Teemischung" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Den Tee kann man sich im <span class="highlight">Teehaus</span> seines Vertrauens mischen lassen.</li>
+        <li>Beim <span class="highlight">Kardamom</span> empfehle ich Samen. Ansonsten kann man auch Hülsen verwenden und die jeweils einzeln leicht aufbrechen. Pulver sollte man nicht nehmen.</li>
+        <li>Auf eine Kanne (1l Wasser) <span class="highlight">3TL Tee</span> verwenden und für <span class="highlight">mindestens 3 Minuten</span> ziehen lassen.</li>
+    </ol>
+    
+</div>
