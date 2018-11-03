d7ad232d018b02a4ad26bd3432ae2c3a3ac51d61
just-me- <hess-marcel@bluewin.ch>
2018-11-03 22:29:27 +0100

    More fancy menu on mobile

--- templates/wuza.php
+++ templates/wuza.php
@@ -68,9 +68,18 @@
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <span class="sr-only">Navigation</span>
+          <!--
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
+          -->
+          <div id="menu-border">
+            <svg class="ham hamRotate" viewBox="0 0 100 100" width="40" height="40" onclick="">
+              <path class="line top" d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
+              <path class="line middle" d="m 30,50 h 40" />
+              <path class="line bottom" d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
+            </svg>
+          </div>
         </button>
         <a class="navbar-brand no-padding no-margin" href="<?php echo $this->_['url']; ?>">
           <!--
