2388b7e738a89ce61e3b38f8ef2f17cddd0cd352
just-me- <hess-marcel@bluewin.ch>
2018-11-03 12:14:58 +0100

    Rezept: French Toast

---
+++ templates/recipe_french_toast.php
@@ -0,0 +1,41 @@
+<div id="main_recipe_french_toast" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">French Toast</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Toast- oder Weissbrot</li>
+                <li>Eier</li>
+                <li>Etwas Milch</li>
+                <li>Butter zum Anbraten</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/french_toast_main.jpg" alt="French Toast" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li><span class="highlight">Eier</span> in einen flachen Teller aufschlagen.</li>
+        <li>Pro Ei ein <span class="highlight">EL Milch</span> zugeben. Gut verrühren.</li>
+        <li>Mit etwas <span class="highlight">Pfeffer und Salz</span> verfeinern.</li>
+        <li>Die <span class="highlight">Brotscheiben</span> beidseitig in die Mischung tunken, dann mit etwas <span class="highlight">Butter</span> beidseitig goldig anbraten.</li>
+    </ol>
+    
+</div>
