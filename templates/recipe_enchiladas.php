0d2562f1cfe1d6937223ab0ac0bb30e217dce811
just-me- <hess-marcel@bluewin.ch>
2018-11-03 18:53:31 +0100

    Rezept Enchiladas

---
+++ templates/recipe_enchiladas.php
@@ -0,0 +1,72 @@
+<div id="main_recipe_enchiladas" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Enchiladas</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>400g Pouletgeschnetzeltes</li>
+                <li>2 Zwiebeln</li>
+                <li>2 Knoblauchzehen</li>
+                <li>2 Peperoni</li>
+                <li>1 Chilischote</li>
+                <li>400g Tomatensauce</li>
+                <li>1 Dose Kidneybohnen</li>
+                <li>1 Dose Mais</li>
+                <li>Tortillas</li>
+                <li>120g Käsekuchenmischung</li>
+                <li>Crème fraîche zum Servieren</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_main.jpg" alt="Enchiladas" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_step1.jpg" alt="Schritt 1" />
+                    <span class="title">Füllung</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_step2.jpg" alt="Schritt 2" />
+                    <span class="title">Tortillas befüllen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_step3.jpg" alt="Schritt 3" />
+                    <span class="title">In einer Backform anrichten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_step4.jpg" alt="Schritt 4" />
+                    <span class="title">Restliche Füllung und Käse hinzugeben</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_step5.jpg" alt="Schritt 5" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/enchiladas_end.jpg" alt="Enchiladas aus Teller" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li><span class="highlight">Poulet</span> in etwas Öl anbraten. Gemüse schneiden.</li>
+        <li><span class="highlight">Zwiebeln, Knoblauch, Peperoni</span> und <span class="highlight">Chilli</span> mitdünsten.</li>
+        <li><span class="highlight">Tomatensauce, Mais</span> und <span class="highlight">Bohnen</span> hinzugeben.</li>
+        <li>Mit <span class="highlight">Salz, Pfeffer und Paprikapulver</span> abschmecken. Koriander und Petersilie passen auch sehr gut.</li>
+        <li>Die <span class="highlight">Tortillas</span> rollen und in einer Ofenform anrichten. Restliche Sauce und Käse darüber verteilen.</li>
+        <li>Im Ofen bei <span class="highlight">200 °C für 25 Minuten</span> backen.</li>
+    </ol>
+    
+</div>
