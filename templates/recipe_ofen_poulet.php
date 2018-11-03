00a14f08fef53b20913302cfc7c93614819f2402
just-me- <hess-marcel@bluewin.ch>
2018-11-03 12:27:38 +0100

    Rezept: Poulet aus dem Ofen

---
+++ templates/recipe_ofen_poulet.php
@@ -0,0 +1,40 @@
+<div id="main_recipe_ofen_poulet" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Poulet aus dem Ofen</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Die Zutaten für die Marinade folgen später.</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/ofen_poulet_main.jpg" alt="Poulet aus dem Ofen" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/ofen_poulet_end.jpg" alt="Knuspriges Poulet" />
+                    <span class="title">Knusprig und lecker</span>
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Das Poulet beidseitig nach belieben würzen und allenfalls mit Öl bestreichen.</li>
+        <li>Bei <span class="highlight">180 °C Umluft</span> für je 30 Minuten pro Seite in den Ofen.</li>
+    </ol>
+    
+</div>
