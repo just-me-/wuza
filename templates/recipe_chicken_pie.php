641475325fd7606e3a0a62576fe066cafc994631
just-me- <hess-marcel@bluewin.ch>
2018-11-03 21:10:49 +0100

    Rezept Basic Chicken Pie

---
+++ templates/recipe_chicken_pie.php
@@ -0,0 +1,53 @@
+<div id="main_recipe_chicken_pie" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Chicken Pie</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Die Zutaten für dieses Rezept folgen später.</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_main.jpg" alt="Chicken Pie" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_step1.jpg" alt="Step 1" />
+                    <span class="title">Fleisch mit Zwiebeln anbraten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_step2.jpg" alt="Step 2" />
+                    <span class="title">In der Ofenform</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_step3.jpg" alt="Step 3" />
+                    <span class="title">Mit dem Teig bedecken</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_end.jpg" alt="Step 4" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/chicken_pie_end2.jpg" alt="Gebacken" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Die Zubereitungsschritte für dieses Rezept folgen später.</li>
+    </ol>
+    
+</div>
