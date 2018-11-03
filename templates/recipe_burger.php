c837a86a927177fef219156ba81451d734b99359
just-me- <hess-marcel@bluewin.ch>
2018-11-03 21:13:25 +0100

    Rezept Gerüst Burger

---
+++ templates/recipe_burger.php
@@ -0,0 +1,58 @@
+<div id="main_recipe_burger" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Burger mit Country Cuts</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Die Zutaten für dieses Rezept folgen später.</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_main.jpg" alt="Burger" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_step1.jpg" alt="Step 1" />
+                    <span class="title">Kartoffeln spalten und mit Öl mischen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_step2.jpg" alt="Step 2" />
+                    <span class="title">Auf einem Backblech verteilen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_step3.jpg" alt="Step 3" />
+                    <span class="title">Hack mit Zwiebeln vermischen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_step4.jpg" alt="Step 4" />
+                    <span class="title">Beidseitig anbraten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_step5.jpg" alt="Step 5" />
+                    <span class="title">Burger belegen</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/burger_end.jpg" alt="Angerichtet" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Die Zubereitungsschritte für dieses Rezept folgen später.</li>
+    </ol>
+    
+</div>
