e0a4e797ee559cc4d6242e1d70b941cf6fd223f9
just-me- <hess-marcel@bluewin.ch>
2019-06-10 23:49:17 +0200

    Gerüst für die restlichen Rezepte erfasst

---
+++ templates/recipe_curry.php
@@ -0,0 +1,35 @@
+<div id="main_recipe_curry" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Curry</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Coming soon</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/curry_main.jpg" alt="Curry" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li><span class="highlight">Coming soon</span></li>
+    </ol>
+    
+</div>
---
+++ templates/recipe_lasagne.php
@@ -0,0 +1,35 @@
+<div id="main_recipe_lasagne" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Lasagne</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>Coming soon</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_main.jpg" alt="Lasagne" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li><span class="highlight">Coming soon</span></li>
+    </ol>
+    
+</div>
