a4fe5daae722bf1d87f7a8d30452de9c8dfe346e
just-me- <hess-marcel@bluewin.ch>
2019-06-10 20:54:03 +0200

    Rezept: Züri-Döner

---
+++ templates/recipe_zueri_doener.php
@@ -0,0 +1,63 @@
+<div id="main_recipe_zueri_doener" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Züri-Döner</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten</h2>
+            <ul class="ingredients checkable noselect">
+                <li>600g Kalbsgeschnetzeltes</li>
+                <li>1 EL Mehl</li>
+                <li>1 Zwiebel</li>
+                <li>3 Paprika</li>
+                <li>1 dl Weisswein oder Whiskey</li>
+                <lI>2 dl Vollrahm</lI>
+                <li>1 dl Fleischbouillon</li>
+                <li>1 EL Maisstärke</li>
+                <li>Dose Champignons</li>
+                <li>Rösti (Fertigpackung)</li>
+                <li>Weizentortillas</li>
+                <li>Etwas Petersilie</li>
+                <li>Naturjoghurt zum Servieren</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/zueri_doener_main.jpg" alt="Züri-Döner" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/zueri_doener_step1.jpg" alt="Rösti" />
+                    <span class="title">Rösti anbraten</span>
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/zueri_doener_end.jpg" alt="Zürcher Geschnetzeltes" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect_">
+        <li>
+            Das <span class="highlight">Fleisch mit Butter</span> anbraten.
+            Mit wenig <span class="highlight">Mehl</span> bestreuen und mit <span class="highlight">Salz und Pfeffer</span> würzen.
+        </li>
+        <li>Das Fleisch herausnehmen. Nun die <span class="highlight">Zwiebeln und Paprikas</span> mit etwas Öl anbraten.</li>
+        <li>Mit dem <span class="highlight">Weisswein oder Whiskey</span> ablöschen. Den Alkohol etwas einkochen lassen.</li>
+        <li>Die <span class="highlight">Meisstärke mit der Bouillon</span> anrühren. Gemisch und den <span class="highlight">Rahm</span> in die Pfanne geben. Aufkochen.</li>
+        <li>Die Hitze reduzieren. Das Fleisch und <span class="highlight">Champignons</span> beigeben.</li>
+        <li><span class="highlight">Rösti</span> gemäss Packungsbeilage anbraten. <span class="highlight">Tortillas</span> im Ofen oder der Mikrowelle warm machen.</li>
+        <li>Servieren. Die Tortillas mit Joghurt bestreichen und mit Rösti und Fleisch befüllen. Etwas Petersilie beilegen. En Guete!</li>
+    </ol>
+    
+</div>
