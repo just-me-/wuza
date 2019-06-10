852934ea37721093478a634018f339599dcd9746
just-me- <hess-marcel@bluewin.ch>
2019-06-10 12:35:36 +0200

    Rezept - Lebkuchen Zuckerguss

--- templates/recipe_lebkuchen.php
+++ templates/recipe_lebkuchen.php
@@ -17,6 +17,13 @@
                 <li>6dl Milch</li>
                 <li>Oblaten / Esspapier für den Boden</li>
             </ul>
+            
+            <h3>Optionaler Zuckerguss</h3>
+            <ul class="ingredients checkable noselect">
+                <li>1 Ei</li>
+                <li>160g Puderzucker</li>
+                <li>Allenfalls Lebensmittelfarbe</li>
+            </ul>
         </div>
         
         <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
@@ -34,6 +41,13 @@
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lebkuchen_main2.jpg" alt="Lebkuchen" />
                 </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lebkuchen3.jpg" alt="Zuckergusssur" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lebkuchen4.jpg" alt="Farbiger Zuckerguss" />
+                    <span class="title">Zuckerguss mit Lebensmittelfarbe</span>
+                </div>
             </div>
             <!-- pagination -->
             <div class="swiper-pagination"></div>
@@ -50,6 +64,14 @@
         <li>Das Blech mit <span class="highlight">Backpapier</span> auslegen und dann den Boden mit <span class="highlight">Oblaten</span> überdecken. Die Mischung darübergiessen.</li>
         <li>Bei <span class="highlight">200 °C  für 25-30 Minuten</span> backen.</li>
         <li>Auskühlen lassen und in Formen schneiden.</li>
+    </ol>
+    
+    <h3>Optionaler Zuckerguss</h3>
+    <ol class="steps noselect">
+        <li>Das <span class="highlight">Eiweiss</span> vom Ei trennen.</li>
+        <li>Das Eiweiss mit einer <span class="highlight">Prise Salz</span> steif schlagen.</li>
+        <li>Den gesiebten <span class="highlight">Puderzucker</span> einrieseln lassen bis eine spritzfähige Masse entsteht.</li>
+        <li>Die Masse in einen <span class="highlight">Spritzbeutel</span> geben und den Lebkuchen verzieren. Gut austrocknen lassen.</li>
     </ol>
     
 </div>
