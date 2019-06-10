9af1cc0b37667d94a5f64a48692a05311ceddb82
just-me- <hess-marcel@bluewin.ch>
2019-06-10 19:15:09 +0200

    Rezept - Basmati

---
+++ templates/recipe_basmati_reis.php
@@ -0,0 +1,43 @@
+<div id="main_recipe_basmati_reis" class="fade-in-move-down">
+    
+    <h1><span class="bubbles">Basmati-Reis (ohne Reiskocher)</span></h1>
+    
+    <div class="recip_wrapper">
+        
+        <div class="col-md-6 no-padding-left">
+            <h2>Zutaten (für 2 Portionen)</h2>
+            <ul class="ingredients checkable noselect">
+                <li>1 Tasse Reis</li>
+                <li>2 Tassen Wasser</li>
+                <li>½ TL Salz</li>
+            </ul>
+        </div>
+        
+        <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/basmati_reis_main.jpg" alt="Basmati-Reis" />
+                </div>
+            </div>
+            <!-- pagination -->
+            <div class="swiper-pagination"></div>
+            
+            <!-- navigation buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    
+    <h2>Zubereitung</h2>
+    <ol class="steps noselect">
+        <li>Den <span class="highlight">Reis</span> gut in warmen Wasser waschen.</li>
+        <li>Das Wasser vom Waschen gut ableeren und das abgemessene <span class="highlight">Wasser</span> in einen Kochtopf mit dem Reis geben.</li>
+        <li>Auf voller Flamme das Wasser <span class="highlight">zum leichten Kochen</span> bringen.</li>
+        <li>
+            <span class="highlight">Salz</span> hinzugeben. <span class="highlight">Hitze abstellen</span>.
+            <span class="highlight">Deckel</span> drauf und <span class="highlight">15 Minuten</span> auf der Platte stehen lassen.
+        </li>
+    </ol>
+    
+</div>
