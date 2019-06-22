954f2603e883ed8b2ec3d628dff5a577d33b783b
just-me- <hess-marcel@bluewin.ch>
2019-06-22 11:35:47 +0200

    Bilder Lasagne

--- templates/recipe_lasagne.php
+++ templates/recipe_lasagne.php
@@ -9,6 +9,7 @@
             <ul class="ingredients checkable noselect">
                 <li>Coming soon</li>
             </ul>
+            <p>Das Rezept für die Bolognese findest Du <a href="<?php echo $this->_['url'].'view/recipe_bolognese'; ?>">hier</a>.</p>
         </div>
         
         <div id="main_recipe_slider" class="swiper-container swiper-slider-no-auto col-md-6 no-padding-left no-padding-right noselect">
@@ -16,6 +17,18 @@
                 <!-- Slides -->
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_main.jpg" alt="Lasagne" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step1.jpg" alt="Butter anrühren" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step2.jpg" alt="Käsesauce" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step3.jpg" alt="Zwiebeln und Knoblauch" />
+                </div>
+                <div class="swiper-slide">
+                    <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_end.jpg" alt="Lasagne aus dem Ofen" />
                 </div>
             </div>
             <!-- pagination -->
