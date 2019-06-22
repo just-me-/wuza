e27b7ec3a7adc1b3885930a41bf55c353593f192
just-me- <hess-marcel@bluewin.ch>
2019-06-22 15:37:08 +0200

    Rezept Lasagne

--- templates/recipe_lasagne.php
+++ templates/recipe_lasagne.php
@@ -7,7 +7,13 @@
         <div class="col-md-6 no-padding-left">
             <h2>Zutaten</h2>
             <ul class="ingredients checkable noselect">
-                <li>Coming soon</li>
+                <li>2EL Mehl</li>
+                <li>2EL Butter</li>
+                <li>1l Milch</li>
+                <li>2 Zwiebeln</li>
+                <li>3 Knoblauchzehen</li>
+                <li>300g - 400g Käsemischung</li>
+                <li>Lasagneblätter</li>
             </ul>
             <p>Das Rezept für die Bolognese findest Du <a href="<?php echo $this->_['url'].'view/recipe_bolognese'; ?>">hier</a>.</p>
         </div>
@@ -20,15 +26,19 @@
                 </div>
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step1.jpg" alt="Butter anrühren" />
+                    <span class="title">Mehl und Butter schmelzen</span>
                 </div>
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step2.jpg" alt="Käsesauce" />
+                    <span class="title">Käsesauce - dauernd umrühren!</span>
                 </div>
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_step3.jpg" alt="Zwiebeln und Knoblauch" />
+                    <span class="title">Zwiebeln und Knoblauch für den Geschmack</span>
                 </div>
                 <div class="swiper-slide">
                     <img src="<?php echo $this->_['url']; ?>img/recipes/lasagne_end.jpg" alt="Lasagne aus dem Ofen" />
+                    <span class="title">Aus dem Ofen - en Guete!</span>
                 </div>
             </div>
             <!-- pagination -->
@@ -42,7 +52,25 @@
     
     <h2>Zubereitung</h2>
     <ol class="steps noselect">
-        <li><span class="highlight">Coming soon</span></li>
+        <li>
+            <span class="highlight">Butter und Mehl</span> im Verhältnis 1:1 in einem Topf - auf maximaler Hitze
+            unter ständigem Umrühren - anbraten bis sich die Butter vollständig gelöst hat.
+        </li>
+        <li>Dann schluckweise die <span class="highlight">Milch</span> hinzugeben, damit eine dicke Konsistenz erzielt wird. Anschliessend nochmals kurz aufkochen und dann weg vom Herd nehmen.</li>
+        <li>
+            <span class="highlight">Zwiebeln</span> vierteln und <span class="highlight">Knoblauch</span> leicht pressen,
+            dann der Masse beifügen. Etwa <span class="highlight">eine Stunde</span> ziehen lassen. Dann Zwiebeln und Knoblauch
+            wieder herausnehmen.
+        </li>
+        <li>Unterdessen die <span class="highlight">Bolognese</span> zubereiten.</li>
+        <li>Nun die <span class="highlight">Käsemischung</span> in die Masse geben und schmelzen wie ein Fondue. Mit <span class="highlight">Salz und Pfeffer</span> würzen.</li>
+        <li>Eine <span class="highlight">Backform</span> mit etwas <span class="highlight">Butter</span> ausreiben.</li>
+        <li>
+            Nun eine Schicht <span class="highlight">Tomaten</span>sauce, dann Lasagne<span class="highlight">blätter</span>
+            und dann <span class="highlight">Käse</span>sauce. Dann wieder Blätter, Tomatensauce, Blätter, Käse, etc.
+            Die oberste Schicht ist <span class="highlight">Käse</span>.
+        </li>
+        <li>Bei <span class="highlight">185 °C für 50 Minuten</span> in den Ofen. </li>
     </ol>
     
 </div>
