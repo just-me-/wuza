f75babed2ae8ab6298d8f7ca41f521e98133884c
just-me- <hess-marcel@bluewin.ch>
2020-02-10 12:04:56 +0100

    SBB Fahrplan

--- templates/project_sbb.php
+++ templates/project_sbb.php
@@ -3,6 +3,15 @@
     <h1>SBB Fahrplan</h1>
     
     <p>
-        Der Inhalt dieses Projekts wird bis Ende Februar aufgeschaltet. 
+        Mittels der SBB API wurde eine kleine, animierte Übersicht der nächsten Bus-Verbindungen
+        von der WG zur HSR und zurück erstellt.
+        Ideal, um sie mit beispielsweise einem Raspberry Pi auf einem Display anzeigen zu lassen.
     </p>
+    
+    <img
+         class="img-responsive blocked_img center-block fade-in-move-down"
+         src="<?php echo $this->_['url']; ?>img/projects/sbb.jpg"
+         alt="Bildschirmfoto SBB"
+         style="max-height: 300px;"
+    />
 </div>
